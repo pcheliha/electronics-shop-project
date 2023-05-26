cd9c0d492b4d5dde772b24520f08a7170714a4f1
Pchel <golovkov_1994@mail.ru>
2023-05-26 18:19:20 +0300

    дописаны методы класса, написаны тесты

--- src/item.py
+++ src/item.py
@@ -13,7 +13,9 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name
+        self.price = price
+        self.quantity = quantity
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +23,12 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        total = self.price * self.quantity
+        return total
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price = self.price * self.pay_rate
+        return self.price
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,19 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+import pytest
+
+from src.item import Item
+
+
+smartphone = Item('Смартфон', 15, 5)
+smartphone.pay_rate = 1.0
+smartphone1 = Item('Ноутбук', 10, 3)
+
+def test_calculate_total_price():
+    assert smartphone.calculate_total_price() == 75
+
+
+def test_apply_discount():
+    assert smartphone.apply_discount() == 15.0
+
+
