52c4ff7b54ac6f02f94c7c7ef865dd4096c338cc
Pchel <golovkov_1994@mail.ru>
2023-05-28 21:58:28 +0300

    добавлены геттеры и сеттеры, добавлен статический метод и класс_метод

--- tests/test_item.py
+++ tests/test_item.py
@@ -7,7 +7,7 @@ from src.item import Item
 
 smartphone = Item('Смартфон', 15, 5)
 smartphone.pay_rate = 1.0
-smartphone1 = Item('Ноутбук', 10, 3)
+smartphone1 = Item('Ноутбук', 10, 3
 
 def test_calculate_total_price():
     assert smartphone.calculate_total_price() == 75
